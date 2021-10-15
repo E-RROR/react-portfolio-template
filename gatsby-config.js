b9ead96a0c6c86d044b631eba47aa80e07a1abdb
Sina Farhadi <ahmadibishop@gmail.com>
2021-10-15 15:42:07 +0330

    change page meta

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    title: `Sina Farhadi Portfolio`,
+    description: `This is Sina Farhadi cool portfolio`,
     author: `@gatsbyjs`,
   },
   plugins: [
